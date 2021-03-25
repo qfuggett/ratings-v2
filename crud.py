fdb3246a557df59902ac0aaa17c24a2654cb5b90
qfuggett <qfuggett@gmail.com>
2021-03-25 15:49:11 +0000

    create crud file to create CRUD functions

---
+++ crud.py
@@ -0,0 +1,19 @@
+"""CRED operations."""
+
+from model import db, User, Movie, Rating, connect_to_db
+
+# CRUD Functions
+def create_user(email, password):
+    """Create and return a new user."""
+
+    user = User(email=email, password=password)
+
+    db.session.add(user)
+    db.session.commit()
+
+    return user
+
+
+if __name__ == '__main__':
+    from server import app
+    connect_to_db(app)
